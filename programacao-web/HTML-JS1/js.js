edb466d303fd600b6c3c7f2abe56801d9208233f
Hugo Maia <maiahugom123@gmail.com>
2022-09-14 17:13:07 -0300

    respondendo q 1 e 2

---
+++ programacao-web/HTML-JS1/js.js
@@ -0,0 +1,14 @@
+
+var button = document.getElementById("carregar");
+
+button.addEventListener("click", function() {
+    
+    var container = document.getElementById("container");
+    if(container.style.display === "none"){
+        container.style.display = "block";
+    } else {
+        container.style.display = "none";
+    }
+
+
+})
