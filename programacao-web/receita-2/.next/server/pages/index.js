31cf9ab58e514e57c745722e523b256705825e32
Hugo Maia <maiahugom123@gmail.com>
2022-11-20 16:30:01 -0300

    iniciando receita 2

---
+++ programacao-web/receita-2/.next/server/pages/index.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Home)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Home() {\n    const estilosDaHomePage = {\n        backgroundColor: \"red\"\n    };\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        style: estilosDaHomePage,\n        children: /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h2\", {\n            children: \"Viva Santana\"\n        }, void 0, false, {\n            fileName: \"/home/hugo/Desktop/GIT/UFRN/programacao-web/receita-1/pages/index.js\",\n            lineNumber: 8,\n            columnNumber: 11\n        }, this)\n    }, void 0, false, {\n        fileName: \"/home/hugo/Desktop/GIT/UFRN/programacao-web/receita-1/pages/index.js\",\n        lineNumber: 6,\n        columnNumber: 8\n    }, this);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTtBQUFlLFNBQVNBLE9BQU07SUFDMUIsTUFBTUMsb0JBQW9CO1FBQUVDLGlCQUFpQjtJQUFNO0lBQ25ELHFCQUdHLDhEQUFDQztRQUFJQyxPQUFRSDtrQkFFViw0RUFBQ0k7c0JBQUc7Ozs7Ozs7Ozs7O0FBVWIsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3JlY2VpdGEtMS8uL3BhZ2VzL2luZGV4LmpzP2JlZTciXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gSG9tZSgpe1xuICAgIGNvbnN0IGVzdGlsb3NEYUhvbWVQYWdlID0geyBiYWNrZ3JvdW5kQ29sb3I6IFwicmVkXCIgfVxuICAgIHJldHVybiAoXG4gXG4gICAgICAgIFxuICAgICAgIDxkaXYgc3R5bGU9IHtlc3RpbG9zRGFIb21lUGFnZX0+XG4gXG4gICAgICAgICAgPGgyPlxuIFxuICAgICAgICAgICAgVml2YSBTYW50YW5hXG4gXG4gICAgICAgICAgPC9oMj5cbiBcbiAgICAgICA8L2Rpdj5cbiBcbiAgICApXG4gXG4gfVxuIl0sIm5hbWVzIjpbIkhvbWUiLCJlc3RpbG9zRGFIb21lUGFnZSIsImJhY2tncm91bmRDb2xvciIsImRpdiIsInN0eWxlIiwiaDIiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ programacao-web/receita-2/pages/index.js
@@ -0,0 +1,18 @@
+export default function Home(){
+    const estilosDaHomePage = { backgroundColor: "red" }
+    return (
+ 
+        
+       <div style= {estilosDaHomePage}>
+ 
+          <h2>
+ 
+            Viva Santana
+ 
+          </h2>
+ 
+       </div>
+ 
+    )
+ 
+ }
